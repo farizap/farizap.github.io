b96d07cda95b26c52072c300d46bf1bb4376bbc9
farizap <farizap@users.noreply.github.com>
2021-02-27 15:57:32 +0000

    Update file(s) "/." from "farizap/Foam"

---
+++ component---src-pages-index-js-8d42d0ecd989aa6d868e.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{RXBc:function(n,e,t){"use strict";t.r(e),t.d(e,"default",(function(){return l}));var r=t("q1tI"),a=t.n(r);function l(){return a.a.createElement(a.a.Fragment,null,a.a.createElement("h1",null,"Hello World!"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-8d42d0ecd989aa6d868e.js.map
